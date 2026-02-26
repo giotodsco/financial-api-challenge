2271b2e65275348246098e76c3c02fb5b4c0b886
giotodsco <todescogiovanni18@gmail.com>
2026-02-26 16:54:25 -0300

    fix: add @Repository annotation to TransacaoRepository bean

--- src/main/java/financial/dev/financial_api_challenge/repository/TransacaoRepository.java
+++ src/main/java/financial/dev/financial_api_challenge/repository/TransacaoRepository.java
@@ -1,10 +1,12 @@
 package financial.dev.financial_api_challenge.repository;
 
 import financial.dev.financial_api_challenge.dtos.TransacaoRequestDTO;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class TransacaoRepository {
 
     List<TransacaoRequestDTO> transacaoRequestDTOS = new ArrayList<>();
