33a2f0dfd4cf9311a4e3f0870957bb2ce239b023
giotodsco <todescogiovanni18@gmail.com>
2026-02-26 11:06:09 -0300

    feat: initial project setup

---
+++ src/main/java/financial/dev/financial_api_challenge/FinancialApiChallengeApplication.java
@@ -0,0 +1,13 @@
+package financial.dev.financial_api_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialApiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialApiChallengeApplication.class, args);
+	}
+
+}
