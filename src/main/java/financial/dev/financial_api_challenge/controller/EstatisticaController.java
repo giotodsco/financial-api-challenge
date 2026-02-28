de2b751b0840b9ddc82a379c305335c00c778d84
giotodsco <todescogiovanni18@gmail.com>
2026-02-28 12:32:35 -0300

    feat: create EstatisticaController with GET /estatistica endpoint

---
+++ src/main/java/financial/dev/financial_api_challenge/controller/EstatisticaController.java
@@ -0,0 +1,33 @@
+package financial.dev.financial_api_challenge.controller;
+
+import financial.dev.financial_api_challenge.dtos.EstatisticaProperties;
+import financial.dev.financial_api_challenge.repository.TransacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+
+@RestController
+@RequestMapping("/estatistica")
+public class EstatisticaController {
+
+    @Autowired
+    private TransacaoRepository transacaoRepository;
+
+    @Autowired
+    private EstatisticaProperties estatisticaProperties;
+
+    @GetMapping
+    public ResponseEntity pegarEstatistica(){
+
+        final var horaInicial = OffsetDateTime.now()
+                .minusSeconds(estatisticaProperties.segundos());
+
+        return ResponseEntity.status(HttpStatus.OK).body(transacaoRepository.pegarEstatisticas(horaInicial));
+    }
+
+}
