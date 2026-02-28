5c86af9c31fe5ef5047802209540d2867bd687be
giotodsco <todescogiovanni18@gmail.com>
2026-02-28 12:31:24 -0300

    fix: remove @Component from EstatisticaProperties record to fix Integer bean injection error

--- src/main/java/financial/dev/financial_api_challenge/dtos/EstatisticaProperties.java
+++ src/main/java/financial/dev/financial_api_challenge/dtos/EstatisticaProperties.java
@@ -3,7 +3,6 @@ package financial.dev.financial_api_challenge.dtos;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "estatistica")
 public record EstatisticaProperties(Integer segundos) {
 }
