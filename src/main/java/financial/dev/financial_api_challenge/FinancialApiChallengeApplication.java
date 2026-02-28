13dedf58ed5ada7301d084e57405c941460c6c34
giotodsco <todescogiovanni18@gmail.com>
2026-02-28 12:32:51 -0300

    feat: add @EnableConfigurationProperties(EstatisticaProperties.class) to main application class

--- src/main/java/financial/dev/financial_api_challenge/FinancialApiChallengeApplication.java
+++ src/main/java/financial/dev/financial_api_challenge/FinancialApiChallengeApplication.java
@@ -1,9 +1,12 @@
 package financial.dev.financial_api_challenge;
 
+import financial.dev.financial_api_challenge.dtos.EstatisticaProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(EstatisticaProperties.class)
 public class FinancialApiChallengeApplication {
 
 	public static void main(String[] args) {
