7793f8c446702f22afbf0a5f9bd458bff4cb8312
giotodsco <todescogiovanni18@gmail.com>
2026-03-01 13:03:41 -0300

    feat: add @NotNull and @Positive constraints

--- src/main/java/financial/dev/financial_api_challenge/dtos/EstatisticaProperties.java
+++ src/main/java/financial/dev/financial_api_challenge/dtos/EstatisticaProperties.java
@@ -1,8 +1,14 @@
 package financial.dev.financial_api_challenge.dtos;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "estatistica")
-public record EstatisticaProperties(Integer segundos) {
+public record EstatisticaProperties(
+        @NotNull
+        @Positive
+        Integer segundos
+) {
 }
